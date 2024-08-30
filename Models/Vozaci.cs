6015f92349094cf103c3a8b672e2e3bcb44e2be6
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-30 22:36:34 +0200

    Create Vozaci.cs

---
+++ Models/Vozaci.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfikasnostPrijevoza_C__API.Models
+{
+    public class Vozaci
+    {
+        [Key]
+        public int vozac_id { get; set; }
+        public string? ime { get; set; }
+        public string? prezime { get; set; }
+        public DateTime? godina_rodenja { get; set; }
+        public DateTime? istek_ugovora { get; set; }
+    }
+}
