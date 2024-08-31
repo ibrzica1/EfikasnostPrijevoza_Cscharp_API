bce92aefb8963aa8df7b5a5d48311f4078e3a1bb
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-31 21:45:59 +0200

    Create EfikasnostContext.cs

---
+++ Data/EfikasnostContext.cs
@@ -0,0 +1,16 @@
+﻿using EfikasnostPrijevoza_C__API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfikasnostPrijevoza_C__API.Data
+{
+    public class EfikasnostContext:DbContext
+    {
+        public EfikasnostContext(DbContextOptions<EfikasnostContext> opcije) : base(opcije)
+        {
+
+        }
+
+        public DbSet<Kamioni> Kamioni { get; set; }
+        public DbSet<Vozaci> Vozaci { get; set; }
+    }
+}
