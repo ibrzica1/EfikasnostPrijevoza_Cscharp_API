b4c7215dbc852fe520f25d1667a84b83dfe6965b
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-31 21:45:53 +0200

    Create VozacController.cs

---
+++ Controllers/VozacController.cs
@@ -0,0 +1,69 @@
+﻿using EfikasnostPrijevoza_C__API.Data;
+using EfikasnostPrijevoza_C__API.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfikasnostPrijevoza_C__API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VozacController:ControllerBase
+    {
+        private readonly EfikasnostContext _context;
+        public VozacController(EfikasnostContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Vozaci);
+        }
+
+        [HttpGet]
+        [Route("{vozac_id:int}")]
+        public IActionResult GetById(int vozac_id)
+        {
+            return Ok(_context.Vozaci.Find(vozac_id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Vozaci vozac)
+        {
+            _context.Vozaci.Add(vozac);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created, vozac);
+        }
+        [HttpPut]
+        [Route("{vozac_id:int}")]
+        [Produces("application/json")]
+        public IActionResult Put(int vozac_id, Vozaci vozac)
+        {
+            var vozacIzBaze = _context.Vozaci.Find(vozac_id);
+
+            vozacIzBaze.ime = vozac.ime;
+            vozacIzBaze.prezime = vozac.prezime;
+            vozacIzBaze.godina_rodenja = vozac.godina_rodenja;
+            vozacIzBaze.istek_ugovora = vozac.istek_ugovora;
+
+            _context.Vozaci.Update(vozacIzBaze);
+            _context.SaveChanges();
+
+            return Ok(new { poruka = "Uspješno promjenjeno" });
+
+        }
+
+        [HttpDelete]
+        [Route("{vozac_id:int}")]
+        [Produces("application/json")]
+
+        public IActionResult Delete(int vozac_id)
+        {
+            var vozacIzBaze = _context.Vozaci.Find(vozac_id);
+            _context.Vozaci.Remove(vozacIzBaze);
+            _context.SaveChanges();
+            return Ok(new { poruka = "Uspješno obrisano" });
+        }
+    }
+}
