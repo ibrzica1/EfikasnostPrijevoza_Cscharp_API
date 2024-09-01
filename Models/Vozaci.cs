7a4380c782fe6d78a288693b4a3b80a0f9b21758
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-01 20:53:34 +0200

    Update Vozaci.cs

--- Models/Vozaci.cs
+++ Models/Vozaci.cs
@@ -8,7 +8,7 @@ namespace EfikasnostPrijevoza_C__API.Models
         public int vozac_id { get; set; }
         public string? ime { get; set; }
         public string? prezime { get; set; }
-        public DateTime? godina_rodenja { get; set; }
+        public DateTime? datum_rodenja { get; set; }
         public DateTime? istek_ugovora { get; set; }
     }
 }
