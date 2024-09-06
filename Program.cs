345ea45a8b70c3ca2b27b81f07a1a519e4b44586
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-06 19:49:43 +0200

    Produkcija

--- Program.cs
+++ Program.cs
@@ -15,25 +15,42 @@ builder.Services.AddDbContext<EfikasnostContext>(
     {
         opcije.UseSqlServer(builder.Configuration.GetConnectionString("EfikasnostContext"));
     }
-    ); 
+    );
+
+builder.Services.AddCors(opcije =>
+{
+    opcije.AddPolicy("CorsPolicy",
+        builder =>
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(opcije =>
     {
         opcije.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
         opcije.EnableTryItOutByDefault();
     });
-}
+//}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseStaticFiles();
+
+app.UseDefaultFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.UseCors("CorsPolicy");
 
 app.Run();
