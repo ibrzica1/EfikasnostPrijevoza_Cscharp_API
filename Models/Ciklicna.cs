d4508b2ff1f6ed866f9b82229b68418efe7ea1f8
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-09 23:51:01 +0200

    Create Ciklicna.cs

---
+++ Models/Ciklicna.cs
@@ -0,0 +1,11 @@
+﻿namespace EfikasnostPrijevoza_C__API.Models
+{
+    public class Ciklicna
+    {
+        public List<int> Lista { get; set; }
+        /* public bool Gore { get; set; }
+        public bool Dolje { get; set; }
+        public bool Desno { get; set; }
+        public bool Lijevo { get; set; } */
+    }
+}
