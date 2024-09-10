42dc41d3c1bb8db7847dec6cbeb0cff218d1d066
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-10 16:17:48 +0200

    Update CiklicnaController.cs

--- Controllers/CiklicnaController.cs
+++ Controllers/CiklicnaController.cs
@@ -11,6 +11,7 @@ namespace EfikasnostPrijevoza_C__API.Controllers
     [Route("api/v1/[controller]")]
     public class CiklicnaController:ControllerBase
     {
+        Ciklicna ciklicna = new Ciklicna();
 
         [HttpGet]
 
@@ -66,5 +67,78 @@ namespace EfikasnostPrijevoza_C__API.Controllers
             
             return Ok(JsonConvert.SerializeObject(tablica));
         }
+
+        /*[HttpGet]
+
+        public IActionResult Metoda2(int a, int b)
+        {
+            int redak = a;
+            int stupac = b;
+            int redovi = redak * stupac;
+            int[,] tablica = new int[redak, stupac];
+            int minRedak = 0;
+            int maxRedak = redak - 1;
+            int minStupac = 0;
+            int maxStupac = stupac - 1;
+            int broj = 1;
+            string siva = "siva";
+            string crna = "crna";
+            int[,] sivaa = new int[maxRedak, maxStupac];
+            List<Ciklicna> lista = new List<Ciklicna>();
+
+
+            while (broj <= redovi)
+            {
+                Ciklicna ciklicna = new Ciklicna();
+                for (int i = maxStupac; i >= minStupac; i--)
+                {
+                    tablica[maxRedak, i] = broj++;
+
+                    if (tablica == sivaa) { ciklicna.Boja = "siva"; } else { ciklicna.Boja = "crna"; }
+
+                    ciklicna.Broj = broj;
+
+                    if (i == minStupac) { ciklicna.Gore = true; } else { ciklicna.Gore = false; }
+
+                    ciklicna.Dolje = false;
+
+                    if (i == minStupac) { ciklicna.Lijevo = false; } else { ciklicna.Lijevo = true; }
+
+                    if (i == maxStupac) { ciklicna.Desno = false; } else { ciklicna.Desno = true; }
+                    lista.Add(ciklicna);
+                }
+
+                if (broj > redovi)
+                {
+                    break;
+                }
+                maxRedak--;
+                for (int i = maxRedak; i >= minRedak; i--)
+                {
+                    tablica[i, minStupac] = broj++;
+
+                }
+                if (broj > redovi)
+                {
+                    break;
+                }
+                minStupac++;
+                for (int i = minStupac; i <= maxStupac; i++)
+                {
+
+                    tablica[minRedak, i] = broj++;
+                }
+
+                minRedak++;
+                for (int i = minRedak; i <= maxRedak; i++)
+                {
+                    tablica[i, maxStupac] = broj++;
+                }
+                maxStupac--;
+            }
+            return Ok(lista);
+
+
+        }*/
     }
 }
