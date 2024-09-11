aed135553587a1c016461cd9c70c2b17a3b3f049
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-11 22:45:05 +0200

    Update CiklicnaController.cs

--- Controllers/CiklicnaController.cs
+++ Controllers/CiklicnaController.cs
@@ -13,9 +13,9 @@ namespace EfikasnostPrijevoza_C__API.Controllers
     {
         Ciklicna ciklicna = new Ciklicna();
 
-        [HttpGet]
+        [HttpGet("Metoda1")]
 
-        public IActionResult Metoda(int a, int b)
+        public IActionResult Metoda1(int a, int b)
         {
             int redak = a;
             int stupac = b;
@@ -68,7 +68,7 @@ namespace EfikasnostPrijevoza_C__API.Controllers
             return Ok(JsonConvert.SerializeObject(tablica));
         }
 
-        /*[HttpGet]
+        [HttpGet("Metoda2")]
 
         public IActionResult Metoda2(int a, int b)
         {
@@ -80,65 +80,93 @@ namespace EfikasnostPrijevoza_C__API.Controllers
             int maxRedak = redak - 1;
             int minStupac = 0;
             int maxStupac = stupac - 1;
-            int broj = 1;
-            string siva = "siva";
-            string crna = "crna";
-            int[,] sivaa = new int[maxRedak, maxStupac];
+            int broj = 0;
             List<Ciklicna> lista = new List<Ciklicna>();
 
 
             while (broj <= redovi)
             {
-                Ciklicna ciklicna = new Ciklicna();
+
                 for (int i = maxStupac; i >= minStupac; i--)
                 {
                     tablica[maxRedak, i] = broj++;
 
-                    if (tablica == sivaa) { ciklicna.Boja = "siva"; } else { ciklicna.Boja = "crna"; }
-
-                    ciklicna.Broj = broj;
-
-                    if (i == minStupac) { ciklicna.Gore = true; } else { ciklicna.Gore = false; }
-
-                    ciklicna.Dolje = false;
-
-                    if (i == minStupac) { ciklicna.Lijevo = false; } else { ciklicna.Lijevo = true; }
-
-                    if (i == maxStupac) { ciklicna.Desno = false; } else { ciklicna.Desno = true; }
+                    var ciklicna = new Ciklicna();
+                    {
+                        if (broj == 1) { ciklicna.Boja = "siva"; } else { ciklicna.Boja = "crna"; }
+                        ciklicna.Broj = broj;
+                        if (i == minStupac) { ciklicna.Gore = true; } else { ciklicna.Gore = false; }
+                        ciklicna.Dolje = false;
+                        if (i == minStupac) { ciklicna.Lijevo = false; } else { ciklicna.Lijevo = true; }
+                        if (i == maxStupac) { ciklicna.Desno = false; } else { ciklicna.Desno = true; }
+                    }
                     lista.Add(ciklicna);
                 }
 
-                if (broj > redovi)
+                if (broj >= redovi)
                 {
                     break;
                 }
                 maxRedak--;
+
+
                 for (int i = maxRedak; i >= minRedak; i--)
                 {
                     tablica[i, minStupac] = broj++;
 
+                    var ciklicna = new Ciklicna();
+                    {
+                        ciklicna.Boja = "crna";
+                        ciklicna.Broj = broj;
+                        if (i == minRedak) { ciklicna.Gore = false; } else { ciklicna.Gore = true; }
+                        ciklicna.Dolje = true;
+                        ciklicna.Lijevo = false;
+                        if (i == minRedak) { ciklicna.Desno = true; } else { ciklicna.Desno = false; }
+                    }
+                    lista.Add(ciklicna);
                 }
-                if (broj > redovi)
+                if (broj >= redovi)
                 {
                     break;
                 }
                 minStupac++;
                 for (int i = minStupac; i <= maxStupac; i++)
                 {
-
                     tablica[minRedak, i] = broj++;
+
+                    var ciklicna = new Ciklicna();
+                    {
+                        ciklicna.Boja = "crna";
+                        ciklicna.Broj = broj;
+                        ciklicna.Gore = false;
+                        if (i == maxStupac) { ciklicna.Dolje = true; } else { ciklicna.Dolje = false; }
+                        ciklicna.Lijevo = true;
+                        if (i == maxStupac) { ciklicna.Desno = false; } else { ciklicna.Desno = true; };
+                    }
+                    lista.Add(ciklicna);
                 }
 
                 minRedak++;
                 for (int i = minRedak; i <= maxRedak; i++)
                 {
                     tablica[i, maxStupac] = broj++;
+
+                    var ciklicna = new Ciklicna();
+                    {
+                        ciklicna.Boja = "crna";
+                        ciklicna.Broj = broj;
+                        ciklicna.Gore = true;
+                        if (i == maxRedak) { ciklicna.Dolje = false; } else { ciklicna.Dolje = true; }
+                        ciklicna.Desno = false;
+                        if (i == maxRedak) { ciklicna.Lijevo = true; } else { ciklicna.Lijevo = false; }
+                    }
+                    lista.Add(ciklicna);
                 }
                 maxStupac--;
             }
+
             return Ok(lista);
 
-
-        }*/
+        }
     }
 }
